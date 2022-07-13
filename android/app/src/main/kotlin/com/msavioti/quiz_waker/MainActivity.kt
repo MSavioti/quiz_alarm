56ca559184e55c605ba5b18efda75cdc50961f21
angelo-squadra <angelo.savioti@squadra.com.br>
2022-07-13 16:29:56 -0300

    created project

---
+++ android/app/src/main/kotlin/com/msavioti/quiz_waker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.msavioti.quiz_waker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_waker/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
