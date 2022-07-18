ed09dd2dbf7d538f7e50a7c8adda951c7b097253
angelo-squadra <angelo.savioti@squadra.com.br>
2022-07-18 01:08:36 -0300

    created mapping for categories names and ids

---
+++ lib/src/shared/constants/categories_id_name_mapping.dart
@@ -0,0 +1,34 @@
+import 'package:quiz_waker/src/shared/constants/categories_ids_constants.dart';
+import 'package:quiz_waker/src/shared/constants/categories_names_constants.dart';
+
+Map<String, String> categoriesIdNameMapping = {
+  CategoriesIdsConstants.generalKnowledge:
+      CategoriesNamesConstants.generalKnowledge,
+  CategoriesIdsConstants.books: CategoriesNamesConstants.books,
+  CategoriesIdsConstants.film: CategoriesNamesConstants.film,
+  CategoriesIdsConstants.music: CategoriesNamesConstants.music,
+  CategoriesIdsConstants.musicalsAndTheatres:
+      CategoriesNamesConstants.musicalsAndTheatres,
+  CategoriesIdsConstants.television: CategoriesNamesConstants.television,
+  CategoriesIdsConstants.videoGames: CategoriesNamesConstants.videoGames,
+  CategoriesIdsConstants.boardGames: CategoriesNamesConstants.boardGames,
+  CategoriesIdsConstants.scienceAndNature:
+      CategoriesNamesConstants.scienceAndNature,
+  CategoriesIdsConstants.computers: CategoriesNamesConstants.computers,
+  CategoriesIdsConstants.math: CategoriesNamesConstants.math,
+  CategoriesIdsConstants.mythology: CategoriesNamesConstants.mythology,
+  CategoriesIdsConstants.sports: CategoriesNamesConstants.sports,
+  CategoriesIdsConstants.geography: CategoriesNamesConstants.geography,
+  CategoriesIdsConstants.history: CategoriesNamesConstants.history,
+  CategoriesIdsConstants.politics: CategoriesNamesConstants.politics,
+  CategoriesIdsConstants.art: CategoriesNamesConstants.art,
+  CategoriesIdsConstants.celebrities: CategoriesNamesConstants.celebrities,
+  CategoriesIdsConstants.animals: CategoriesNamesConstants.animals,
+  CategoriesIdsConstants.vehicles: CategoriesNamesConstants.vehicles,
+  CategoriesIdsConstants.comics: CategoriesNamesConstants.comics,
+  CategoriesIdsConstants.scienceGadgets:
+      CategoriesNamesConstants.scienceGadgets,
+  CategoriesIdsConstants.animeAndManga: CategoriesNamesConstants.animeAndManga,
+  CategoriesIdsConstants.cartoonAndAnimations:
+      CategoriesNamesConstants.cartoonAndAnimations,
+};
