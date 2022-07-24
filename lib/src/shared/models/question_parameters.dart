ad0f452f075fa137563bb4a602f8cc683ff22b50
angelo-squadra <angelo.savioti@squadra.com.br>
2022-07-24 19:58:52 -0300

    extracted class

---
+++ lib/src/shared/models/question_parameters.dart
@@ -0,0 +1,11 @@
+class QuestionParameters {
+  final int amount;
+  final String category;
+  final String difficulty;
+
+  QuestionParameters({
+    required this.amount,
+    required this.category,
+    required this.difficulty,
+  });
+}
