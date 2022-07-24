55797c3501be55b343313615cdc2385f5fb8c45c
angelo-squadra <angelo.savioti@squadra.com.br>
2022-07-24 19:03:01 -0300

    drafted data sources for get trivia questions

---
+++ lib/src/modules/get_trivia_questions/data/datasources/get_trivia_questions_remote_data_source_impl.dart
@@ -0,0 +1,4 @@
+import 'package:quiz_waker/src/modules/get_trivia_questions/data/datasources/get_trivia_questions_remote_data_source.dart';
+
+class GetTriviaQuestionsRemoteDataSourceImpl
+    implements GetTriviaQuestionsRemoteDataSource {}
