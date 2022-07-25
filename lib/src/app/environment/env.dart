2bd2ddda41d38d0c9283ca0945296c0558c6359b
angelo-squadra <angelo.savioti@squadra.com.br>
2022-07-25 19:30:12 -0300

    added query parameters method generation

--- lib/src/app/environment/env.dart
+++ lib/src/app/environment/env.dart
@@ -1,3 +1,16 @@
 class Env {
   static const String baseUrl = 'https://opentdb.com/api.php';
+
+  static Map<String, dynamic> questionParameters({
+    required int amount,
+    required String category,
+    required String difficulty,
+  }) {
+    return {
+      'amount': amount,
+      'category': category,
+      'difficulty': difficulty,
+      'type': 'multiple',
+    };
+  }
 }
